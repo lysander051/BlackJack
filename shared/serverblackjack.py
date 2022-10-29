53edccd3fd2ed96b2a3df249831beaf035839c6c
Badet Maxime <maxime.badet@etu.univ-orleans.fr>
2022-10-30 01:28:24 +0200

    chmod server

--- shared/serverblackjack.py
+++ shared/serverblackjack.py
@@ -7,17 +7,23 @@ from contextlib import AsyncExitStack
 async def gestionCroupier(reader, writer):
     data = await reader.readline()
     print(data.decode())
-    await writer.write(("Bienvenue sur le serveur blackjack").encode())
+    writer.write(("Bienvenue sur le serveur blackjack").encode())
 
 async def gestionJoueur(reader, writer):
     await writer.write(("Bienvenue joueur".encode()))
 
 
 async def gestionnaire():
+    joueurs = await asyncio.start_server(gestionJoueur, '0.0.0.0', 667)
     croupiers = await asyncio.start_server(gestionCroupier, '0.0.0.0', 668)
     print("Server on")
-    async with croupiers:
-        await croupiers.serve_forever() # handle requests for ever
+    async with stack:
+        await stack.enter_async_context(joueurs)
+        await stack.enter_async_context(croupiers)
+        await asyncio.gather(
+            joueurs.serve_forever(),
+            croupiers.serve_forever(),
+        )
 
 
 if __name__ == "__main__":
