596c05f90b66e78dcbe604a4d22ddf1bbc439c8d
Badet Maxime <maxime.badet@etu.univ-orleans.fr>
2022-10-30 01:37:50 +0200

    chmod server

--- shared/serverblackjack.py
+++ shared/serverblackjack.py
@@ -16,13 +16,9 @@ async def gestionnaire():
     joueurs = await asyncio.start_server(gestionJoueur, '0.0.0.0', 667)
     croupiers = await asyncio.start_server(gestionCroupier, '0.0.0.0', 668)
     print("Server on")
-    async with AsyncExitStack() as stack:
-        await stack.enter_async_context(joueurs)
-        await stack.enter_async_context(croupiers)
-        await asyncio.gather(
-            joueurs.serve_forever(),
-            croupiers.serve_forever(),
-        )
+    await joueurs.serve_forever(),
+    await croupiers.serve_forever(),
+        
 
 
 if __name__ == "__main__":
